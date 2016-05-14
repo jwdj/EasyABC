6527f23738fbd1459ce617bd825f7a0fb196a4da
JW de Jong <jw_de_jong@yahoo.com>
2016-05-14 17:53:18 +0200

    optimization on syntax coloring
    15% speed increase when scrolling to the bottom of a large abc-file

--- abc_styler.py
+++ abc_styler.py
@@ -18,7 +18,7 @@ class ABCStyler:
         self.e = styled_text_ctrl
         self.STYLE_DEFAULT = 0
         self.STYLE_COMMENT_NORMAL = 1
-        self.STYLE_COMMENT_SPECIAL = 2        
+        self.STYLE_COMMENT_SPECIAL = 2
         self.STYLE_GRACE = 3
         self.STYLE_FIELD = 4
         self.STYLE_FIELD_VALUE = 5
@@ -26,21 +26,13 @@ class ABCStyler:
         self.STYLE_EMBEDDED_FIELD = 7
         self.STYLE_EMBEDDED_FIELD_VALUE = 8
         self.STYLE_BAR = 9
-#Style ornament was defined twice: remove first definition
-#        self.STYLE_ORNAMENT = 10
         self.STYLE_STRING = 11
         self.STYLE_ORNAMENT_EXCL = 12
         self.STYLE_ORNAMENT_PLUS = 13
         self.STYLE_ORNAMENT = 14
         self.STYLE_LYRICS = 15
 
-    def ColorTo(self, pos, style):        
-        num_chars = pos - self.styling_startpos + 1    
-        if num_chars > 0:
-            self.e.SetStyling(num_chars, style)
-            self.styling_startpos = pos+1
-
-    def OnStyleNeeded(self, event):        
+    def OnStyleNeeded(self, event):
         STYLE_DEFAULT = self.STYLE_DEFAULT
         STYLE_COMMENT_NORMAL = self.STYLE_COMMENT_NORMAL
         STYLE_COMMENT_SPECIAL = self.STYLE_COMMENT_SPECIAL
@@ -57,116 +49,98 @@ class ABCStyler:
         STYLE_ORNAMENT = self.STYLE_ORNAMENT
         STYLE_LYRICS = self.STYLE_LYRICS
         editor = self.e
-        color_to = self.ColorTo
         get_char_at = editor.GetCharAt
+        set_styling = editor.SetStyling
         start = editor.GetEndStyled()    # this is the first character that needs styling
         end = event.GetPosition()        # this is the last character that needs styling        
-        start = editor.PositionFromLine(editor.LineFromPosition(start))
+        line_start = editor.LineFromPosition(start)
+        start = editor.PositionFromLine(line_start)
         state = editor.GetStyleAt(start-1)  # init style
         lengthDoc = end + 1
-        self.styling_startpos = start
         editor.StartStyling(start, 31)   # only style the text style bits
         i = start
         ch = chr(get_char_at(i))
         chNext = chr(get_char_at(i+1))
         chPrev = chr(get_char_at(i-1))
+        char_count = 0
         if chPrev == '}':
             state = STYLE_DEFAULT  # why?
         while i <= lengthDoc:
-            advance = True
-            if state == STYLE_DEFAULT:
+            old_state = state
+
+            if state == STYLE_BAR:
+                if ch not in '|[]:1234':
+                    state = STYLE_DEFAULT
+
+            if ch in '\r\n':  # go back to default style if next character is \r\n (to avoid some strange syntax highlighting whith lyrics at least on mac version)
+                state = STYLE_DEFAULT
+            elif state == STYLE_DEFAULT:
                 if (ch == '|' or (ch == ':' and chNext in '|:')) or (ch == '[' and chNext in '1234'):
-                    color_to(i-1, state)
                     state = STYLE_BAR
                 elif chPrev in '\r\n[\x00' and ch in 'ABCDEFGHIJKLMmNOPQRrSsTUVWwXYZ' and chNext == ':':
-                    color_to(i-1, state)
                     if ch == 'X':
                         state = STYLE_FIELD_INDEX
-                    elif chPrev in '\r\n' and ch in ('w', 'W'):
-                        state = STYLE_LYRICS
                     elif chPrev in '\r\n':
-                        state = STYLE_FIELD
+                        if ch in ('w', 'W'):
+                            state = STYLE_LYRICS
+                        else:
+                            state = STYLE_FIELD
                     elif chNext != '[':
                         state = STYLE_EMBEDDED_FIELD   # field on the [M:3/4] form
                     else:
                         state = STYLE_DEFAULT
                 elif ch == '!':
-                    color_to(i-1, state)
                     state = STYLE_ORNAMENT_EXCL
                 elif ch == '+':
-                    color_to(i-1, state)
                     state = STYLE_ORNAMENT_PLUS
                 elif ch == '%' and chNext != '%':
-                    color_to(i-1, state)
                     state = STYLE_COMMENT_NORMAL
                 elif ch == '%' and chNext == '%':
-                    color_to(i-1, state)
                     state = STYLE_COMMENT_SPECIAL
                 elif ch == '"':
-                    color_to(i-1, state)
                     state = STYLE_STRING
                 elif ch == '{':
-                    color_to(i-1, state)
                     state = STYLE_GRACE
                 elif ch in 'HIJKLMNOPQRSTUVWhijklmnopqrstuvw~':
-                    color_to(i-1, state)
-                    color_to(i, STYLE_ORNAMENT)
+                    state = STYLE_ORNAMENT
+            elif state == STYLE_ORNAMENT:
+                state = STYLE_DEFAULT
+            elif state == STYLE_FIELD_VALUE:
+                if ch == '%' and chPrev != '\\':
+                    state = STYLE_COMMENT_NORMAL
+            elif state == STYLE_EMBEDDED_FIELD_VALUE:
+                if ch == ']':
                     state = STYLE_DEFAULT
-            elif state == STYLE_BAR:
-                if ch not in '|[]:1234':
-                    color_to(i-1, state)
-                    state = STYLE_DEFAULT
-                    advance = False
-            elif state in [STYLE_FIELD, STYLE_EMBEDDED_FIELD, STYLE_FIELD_INDEX, STYLE_FIELD_VALUE, STYLE_EMBEDDED_FIELD_VALUE, STYLE_COMMENT_NORMAL, STYLE_COMMENT_SPECIAL]:
-#Do not check for ] in other case thant STYLE EMBEDDED to avoid going in default style if used in comments or commands like %%staves/%%scores
-#                if ch in '\r\n]' or (state == STYLE_EMBEDDED_FIELD_VALUE and ch == ']'):
-                if ch in '\r\n' or (state == STYLE_EMBEDDED_FIELD_VALUE and ch == ']'):
-                    color_to(i-1, state)
-                    state = STYLE_DEFAULT
-                    color_to(i, state)
-                elif state == STYLE_FIELD and ch == ':':
-                    color_to(i, state)
+            elif state == STYLE_FIELD:
+                if ch == ':':
                     state = STYLE_FIELD_VALUE
-                elif state == STYLE_EMBEDDED_FIELD and ch == ':':
-                    color_to(i, state)
+            elif state == STYLE_EMBEDDED_FIELD:
+                if ch == ':':
                     state = STYLE_EMBEDDED_FIELD_VALUE
-                #elif state == STYLE_EMBEDDED_FIELD_VALUE and ch == ']':
-                #    color_to(i, state)
-                #    state = STYLE_DEFAULT
-
             elif state == STYLE_STRING:
-                if ch == '"' and chPrev != '\\' or ch in '\r\n':
-                    color_to(i, state)
-                    state = STYLE_DEFAULT
-            elif state == STYLE_LYRICS:
-                if ch in '\r\n':
-                    color_to(i, state)
+                if ch == '"' and chPrev != '\\':
                     state = STYLE_DEFAULT
             elif state == STYLE_GRACE:
                 if ch == '}':
-                    color_to(i, state)
                     state = STYLE_DEFAULT
             elif state == STYLE_ORNAMENT_EXCL:
-                if ch in '!\r\n':
-                    color_to(i, state)
+                if ch == '!':
                     state = STYLE_DEFAULT
             elif state == STYLE_ORNAMENT_PLUS:
-                if ch in '+\r\n':
-                    color_to(i, state)
+                if ch == '+':
                     state = STYLE_DEFAULT
-#force to go back to STYLE_DEFAULT if in none of the previous case
-            else:
-                state = STYLE_DEFAULT
+            #elif state not in [STYLE_LYRICS, STYLE_BAR, STYLE_COMMENT_NORMAL, STYLE_COMMENT_SPECIAL, STYLE_FIELD_INDEX]:
+            #    state = STYLE_DEFAULT  # force to go back to STYLE_DEFAULT if in none of the previous case
 
-#go back to default style if next character is \r\n (to avoid some strange syntax highlighting whith lyrics at least on mac version)
-            if chNext in '\r\n':
-                self.ColorTo(i, state)
-                state = self.STYLE_DEFAULT
+            if old_state != state: # and char_count > 0:
+                set_styling(char_count, old_state)
+                char_count = 0
 
-            if advance:
-                i += 1
-                chPrev = ch
-                ch = chNext
-                chNext = chr(get_char_at(i+1))
+            char_count += 1
+            i += 1
+            chPrev = ch
+            ch = chNext
+            chNext = chr(get_char_at(i+1))
 
-        color_to(end, state)
+        if char_count > 0:
+            set_styling(char_count, old_state)
